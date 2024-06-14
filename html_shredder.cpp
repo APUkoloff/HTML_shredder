f34af0f0f29d3bad67127032d9bdeb71165cb4b7
APUkoloff <159104457+APUkoloff@users.noreply.github.com>
2024-06-14 21:37:03 +0700

    Add files via upload
    new stuff

--- html_shredder.cpp
+++ html_shredder.cpp
@@ -1,3 +1,4 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +7,8 @@
 #define MAX_POSSIBLE 16384
 
 static int start = -1;
+static int LIMIT = -1;
+static int util_str_index = -1;
 
 struct Stack
 {
@@ -31,13 +34,41 @@ char Pop(Stack& my_stack) // reduce stack & return data function
     return my_stack.text[my_stack.size];
 }
 
-int backup_piece(char str[]) 
+void dump_full_message(FILE* f, int fsize) 
 {
+    fclose(f);
+    f = fopen("source.html", "r");
+    if ((fsize <= 0)||(f == NULL))
+    {
+        printf("Empty file or internal error");
+    }
+    else 
+    {
+        char* content = (char*)malloc(fsize * sizeof(char));
+        fread(content, sizeof(char), fsize, f);
+        if (content != NULL)
+        {
+            puts(content);
+        }
+        free(content);
+    }
+    fclose(f);
+}
+
+int backup_piece(char str[], int end) 
+{
+    if (util_str_index < 0)
+        return 0;
+    int offset = util_str_index;
     FILE* out;
     out = fopen("msg_split_temp.txt", "w");
-    fputs(str, out);
-    int len = strlen(str);
-    return len;
+    for (int j = offset; j < end; j++) 
+    {
+        fprintf_s(out, "%c", str[j]);
+    }
+    fprintf_s(out, "\0");
+    fprintf_s(out, "\n");
+    return end-offset;
 }
 
 int load_piece(int len)
@@ -55,46 +86,99 @@ int load_piece(int len)
     }
 }
 
-int check_piece(int start, FILE *f, int fsize, int LIMIT) 
+int opening_tag_found(int works, int i, FILE *f, int fsize, char workable[], Stack S) 
 {
     char tag_open = '<' ; // opening brackets
     char tag_close = '>' ; // closing brackets
-    int works;
-    char* workable = (char*)malloc(LIMIT*sizeof(char));
-    Stack S;
-    S.size = 0;
     char stack_top;
-    int i, j;
-    works = 1;
-    i = start;
-    while (works && (i<fsize))
+    util_str_index = i;
+    while (works && (i < fsize))
     {
         fscanf_s(f, "%c", &workable[i]);
-        if (works && (i > 1))
-            backup_piece(workable);
         if (workable[i] != '\0')
         {
-            for (j = 0; j < 3; j++) // check brackets
+            if (workable[i] == tag_open) // opening bracket found
             {
-                if (workable[i] == tag_open) // opening bracket found
-                {
-                    Push(S, workable[i], LIMIT); //add to stack - push this char in array inside
-                    break;
-                }
-                if (workable[i] == tag_close) // closing bracket found
-                {
-                    stack_top = Pop(S); // remove that opening bracket(see above) from stack. reduce stack size.
-                    if (stack_top != tag_open)
-                        works = 0; // will happen when a)brackets are not paired at all b) closing bracket of pair came before the opening one
-                    break;
-                }
+                Push(S, workable[i], LIMIT); //add to stack - push this char in array inside
+                break;
+            }
+            if (workable[i] == tag_close) // closing bracket found
+            {
+                stack_top = Pop(S); // remove that opening bracket(see above) from stack. reduce stack size.
+                if (stack_top != tag_open)
+                    works = 0; // will happen when a)brackets are not paired at all b) closing bracket of pair came before the opening one
+                break;
             }
             i++;
         }
         else
             break;
     }
-    if (works && (S.size == 0))
+    if (works) 
+        return 1;
+    else 
+    { 
+        util_str_index = -1;
+        return 0; 
+    }
+}
+
+int closing_tag_found(int works, int i, FILE* f, int fsize, char workable[], Stack S)
+{
+    if (works == 0)
+        return 0;
+    char tag_open = '<'; // opening brackets
+    char tag_close = '>'; // closing brackets
+    char stack_top;
+    while (works && (i < fsize))
+    {
+        fscanf_s(f, "%c", &workable[i]);
+        if (works && (i > 1))
+            backup_piece(workable, i);
+        if (workable[i] != '\0')
+        {
+            if (workable[i] == tag_open) // opening bracket found
+            {
+                Push(S, workable[i], LIMIT); //add to stack - push this char in array inside
+                break;
+            }
+            if (workable[i] == tag_close) // closing bracket found
+            {
+                stack_top = Pop(S); // remove that opening bracket(see above) from stack. reduce stack size.
+                if (stack_top != tag_open)
+                    works = 0; // will happen when a)brackets are not paired at all b) closing bracket of pair came before the opening one
+                break;
+            }
+            i++;
+        }
+        else
+            break;
+    }
+    if (works)
+        return 1;
+    else return 0;
+}
+
+int check_piece(int start, FILE *f, int fsize, int LIMIT) 
+{
+    
+    int works;
+    char* workable = (char*)malloc(LIMIT*sizeof(char));
+    Stack S1;
+    Stack S2;
+    S1.size = 0;
+    S2.size = 0;
+    int i, j;
+    works = 1;
+    i = start;
+    if (fsize < LIMIT) 
+    {
+        dump_full_message(f, fsize);
+        return 42;
+    }
+    works = opening_tag_found(works, i, f, fsize, workable, S1);
+    works = closing_tag_found(works, i, f, fsize, workable, S1);
+    if (works && (S1.size == 0))
     {
         if(workable!=NULL)
             puts(workable);
@@ -119,19 +203,28 @@ int msg_split(int LIMIT)
     int res = check_piece(start, f, fsize, LIMIT);
     if (res == 1) 
     {
-        printf("This message length exceeds limit. Splitting.\n");
+        printf("This message length exceeds limit. Splitted.\n");
         //split if possible
     }
+    if (res == 42) 
+    {
+        printf("The message above fullfills size limit and was printed as is\n");
+    }
+    return 0;
 }
 
 int main()
 {
-    int LIMIT = 0;
     do
     {
         scanf_s("%d", &LIMIT);
-        msg_split(LIMIT);
+        
         if (LIMIT <= 0)
             printf("Bad input. Required: positive value");
+        
     } while (LIMIT <= 0);
+    if (msg_split(LIMIT) == 404)
+        printf("Source file not found\n");
+    else
+        printf("Completed\n");
 }
